d43b17e2937f3cf695cfc82186eb6e4934694363
Ayush Goyal <ayush_g@ar.iitr.ac.in>
2024-12-19 18:28:37 +0530

    back to normal

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
